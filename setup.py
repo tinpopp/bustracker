5903001a1f6e58ace13fbbcfa7655cffd4058e0d
Thanathip Limna <boatkrap@gmail.com>
2014-09-22 15:03:02 +0700

    modify setup

--- setup.py
+++ setup.py
@@ -6,10 +6,6 @@ here = os.path.abspath(os.path.dirname(__file__))
 # README = open(os.path.join(here, 'README.rst')).read()
 # CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-
-CONTROLLER = True
-COMPUTE_NODE = True
-
 requires = [
     'eve',
 ]
